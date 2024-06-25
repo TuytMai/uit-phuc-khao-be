0ad718786876228efc72ba086d00b50ddf77fe02
Tuyt Mai <tranthituyetmai7430@gmail.com>
2024-06-25 20:44:30 +0700

    fix

--- src/test-score-review-form/test-score-review-form.service.ts
+++ src/test-score-review-form/test-score-review-form.service.ts
@@ -35,6 +35,9 @@ export class TestScoreReviewFormService {
           id: studentId,
         },
       },
+      relations: {
+        testScore: true,
+      },
       order: {
         ngayDangKy: 'ASC',
       },
